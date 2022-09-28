04362ac4171edb429a7c665612f2e8a0866b2115
Michelle Kaye Lagunsad <mlagunsad@gmail.com>
2022-09-28 11:05:51 -0700

    Bicycle project

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,10 @@ export default function Layout({ children }: any) {
 		<div className={styles.layout}>
 			<div className={styles.legend}>
 				<h3 className={styles.legendHeader}>Projects</h3>
-				<Link href="/projects/basketball">Basketball</Link>
+				<ul className={styles.legendList}>
+					<li><Link href="/projects/basketball">Basketball</Link></li>
+					<li><Link href="/projects/bicycle">Bicycle</Link></li>
+				</ul>
 			</div>
 
 			<main className={styles.layoutMain}>
---
+++ pages/projects/bicycle.tsx
@@ -0,0 +1,14 @@
+import Canvas from '../../components/canvas';
+import Title from '../../components/title';
+import styles from './bicycle.module.scss';
+
+export default function Bicycle() {
+	return (
+		<>
+			<Canvas>
+			</Canvas>
+
+			<Title title="Bicycle Ride" date="Septembers 28, 2022"/>
+		</>
+	)
+}
