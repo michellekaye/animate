aa5c41c88d85479b666f0a73030a5c96321cfe81
Michelle Kaye Lagunsad <mlagunsad@gmail.com>
2022-09-27 22:07:22 -0700

    Basketball bounce

--- pages/projects/basketball.tsx
+++ pages/projects/basketball.tsx
@@ -1,10 +1,22 @@
 import Canvas from '../../components/canvas';
 import Title from '../../components/title';
+import styles from './basketball.module.scss';
 
 export default function Basketball() {
 	return (
 		<>
-			<Canvas></Canvas>
+			<Canvas>
+				<div className={styles.basketballContainer}>
+					<div className={styles.basketball}>
+						<div className={styles.highlight} />
+						<div className={styles.basketballLine1} />
+						<div className={styles.basketballLine2} />
+					</div>
+
+					<div className={styles.shadow} />
+				</div>
+			</Canvas>
+
 			<Title title="Basketball bounce" date="Septembers 27, 2022"/>
 		</>
 	)
